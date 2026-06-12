de1f8df6f0c4c44d6e9c7de0033179d61d107f97
dmitriydrobkin <146424787+dmitriydrobkin@users.noreply.github.com>
2026-06-12 10:28:21 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, // <-- Добавили эту строку
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -20,9 +20,7 @@ export default nextConfig;
 // (note: this check is recommended but completely optional)
 if (process.env.NODE_ENV === 'development') {
   // `await`ing the call is not necessary but it helps making sure that the setup has succeeded.
-  //  If 
-
-you cannot use top level awaits you could use the following to avoid an unhandled rejection:
-  //  `setupDevPlatform().catch(e => console.error(e));`
+  // If you cannot use top level awaits you could use the following to avoid an unhandled rejection:
+  // `setupDevPlatform().catch(e => console.error(e));`
   await setupDevPlatform({ persist: true });
 }
