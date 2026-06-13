8653946f0ba52798efe18f7b8e7108b67d124d36
dmitriydrobkin <146424787+dmitriydrobkin@users.noreply.github.com>
2026-06-13 17:56:34 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Отключаем статику для страниц ошибок, переводим их в Edge
-  experimental: {
-    runtime: 'edge',
-  },
   images: {
     unoptimized: true,
   },
