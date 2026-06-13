81e9a2ada7374efaa6c6a201439b06771f577449
dmitriydrobkin <146424787+dmitriydrobkin@users.noreply.github.com>
2026-06-14 00:48:24 +0300

    Update catalog.ts

--- src/server/functions/catalog.ts
+++ src/server/functions/catalog.ts
@@ -1,22 +1,23 @@
 import { unstable_noStore as noStore } from 'next/cache';
-import { getRequestContext } from '@cloudflare/next-on-pages';
-import { drizzle } from 'drizzle-orm/d1';
+import { db } from '@/server/db';
 import { categories, products } from '@/server/db/schema';
 
 export const runtime = 'edge';
 
 export async function getCategories() {
   noStore();
-  const { env } = getRequestContext();
-  const db = drizzle(env.DB);
-  
-  return await db.select().from(categories).all();
+  try {
+    return await db.select().from(categories).all();
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function getProducts() {
   noStore();
-  const { env } = getRequestContext();
-  const db = drizzle(env.DB);
-  
-  return await db.select().from(products).all();
+  try {
+    return await db.select().from(products).all();
+  } catch (error) {
+    return [];
+  }
 }
