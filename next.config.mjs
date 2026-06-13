daaac60d6fde7d92d4f53bfa8597e36089677b5a
dmitriydrobkin <146424787+dmitriydrobkin@users.noreply.github.com>
2026-06-13 15:59:52 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,12 @@
-// we import the utility from the next-dev submodule
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Отключаем статику для страниц ошибок, переводим их в Edge
+  experimental: {
+    runtime: 'edge',
+  },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
   },
 };
 
 export default nextConfig;
-
-// we only need to use the utility during development so we can check NODE_ENV
-// (note: this check is recommended but completely optional)
-if (process.env.NODE_ENV === 'development') {
-  // `await`ing the call is not necessary but it helps making sure that the setup has succeeded.
-  // If you cannot use top level awaits you could use the following to avoid an unhandled rejection:
-  // `setupDevPlatform().catch(e => console.error(e));`
-  await setupDevPlatform({ persist: true });
-}
